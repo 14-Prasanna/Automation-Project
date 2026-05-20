9cbc1a12cdc8988b330a0801254555e2ec661a47
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2026-05-20 17:13:53 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -16,7 +16,7 @@ import org.testng.annotations.DataProvider;
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "rerun:target/failedrerun.txt"
-          },tags="@Tamil",
+          },tags="@Search or @WishlistFeature or @Smoke",
         monochrome = true
 )
 
