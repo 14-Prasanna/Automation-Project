8598f91a4df03a5d15a5ee23fa57cc2fe5caef12
samihamuhabathulla-jpg <samihamuhabathulla@gmail.com>
2026-05-26 12:52:26 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -17,7 +17,7 @@ import org.testng.annotations.DataProvider;
                 "rerun:target/failedrerun.txt"
                 
         },
-        tags="@BlogFeature",
+        tags="@ShopByCategory",
         monochrome = true
 ) 
 
