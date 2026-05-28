bf62dc6fb842d44df31b5f1f8f48d4605e70b5f2
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-05-28 14:42:30 +0530

    Update TestNgRunner.java

--- src/test/java/com/runner/TestNgRunner.java
+++ src/test/java/com/runner/TestNgRunner.java
@@ -6,7 +6,6 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
 
-<<<<<<< HEAD
 		features = "src/test/resources/features/", 
 		glue = { "com.stepDefinitions", "com.hooks" }, 
 		plugin = { "pretty",
@@ -15,7 +14,7 @@ import org.testng.annotations.DataProvider;
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 				"rerun:target/failedrerun.txt" },
 
-        
+        tags="@Rishwanth",
 
         monochrome = true
 )
